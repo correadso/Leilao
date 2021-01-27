ee7361fa91c7a48e7c7954e78843ceeb829322b2
Diogo <diogocorrea@tecnomotor.com.br>
2021-01-27 00:30:03 -0300

    Aula 2 Criando testes de unidade com Mockito
    - Testes com mockito em ListaLeilaoAdapterTest
    - Testes instrumentados utilizando o device em ExampleInstrumentedTest

--- app/src/androidTest/java/br/com/alura/leilao/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ExampleInstrumentedTest.java
@@ -7,6 +7,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +24,17 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void deve_AtualizarListaDeLeiloes_QuandoRecebeListaDeLeiloes() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("br.com.alura.leilao", appContext.getPackageName());
+        ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(appContext);
+        adapter.atualiza(new ArrayList<>(Arrays.asList(
+                new Leilao("Console"),
+                new Leilao("Computador"))));
+
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+        assertThat(quantidadeLeiloesDevolvida, is(2));
+
     }
 }
---
+++ app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -0,0 +1,67 @@
+package br.com.alura.leilao.ui.recyclerview.adapter;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.alura.leilao.model.Leilao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ListaLeilaoAdapterTest {
+
+    @Test
+    public void deve_AtualizarListaDeLeiloes_QuandoRecebeListaDeLeiloes() {
+        // objeto mock não executa nada da classe que estamos simulando, spy sim
+        Context context = Mockito.mock(Context.class);
+        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
+        Mockito.doNothing().when(adapter).atualizaLista();
+
+        adapter.atualiza(new ArrayList<>(Arrays.asList(
+                new Leilao("Console"),
+                new Leilao("Computador"))));
+
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+
+        Mockito.verify(adapter).atualizaLista();
+        assertThat(quantidadeLeiloesDevolvida, is(2));
+    }
+
+}
